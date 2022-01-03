1f32c661ab898ef46c0c43c10f96f2b8875b05cd
KILLER <78667474+alexander695@users.noreply.github.com>
2022-01-03 00:18:42 -0800

    Update Xhacking.py

--- Xhacking.py
+++ Xhacking.py
@@ -1,37 +1,37 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 import webbrowser
 import os
 import time
 from colorama import Fore
 #banner#
-print (Fore.YELLOW +  '   ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄███████▄')          
-print (Fore.YELLOW + '  ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███')                   
-print (Fore.YELLOW + '  ███    ███     ███    ███ ███    █▀    ███▐██▀   ███▌ ███   ███   ███    █▀')            
-print (Fore.YELLOW + ' ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀    ███▌ ███   ███  ▄███      ')                      
-print (Fore.YELLOW + '▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄')                
-print (Fore.YELLOW + '  ███    ███     ███    ███ ███    █▄    ███▐██▄   ███  ███   ███   ███    ███')                                
-print (Fore.YELLOW + '  ███    ███     ███    ███ ███    ███   ███ ▀███▄ ███  ███   ███   ███    ███')                    
-print (Fore.YELLOW + '  ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀') 
-print ("CODED BY KILLER")
-
 def menu():
+    print (Fore.YELLOW +  '   ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄███████▄')          
+    print (Fore.YELLOW + '  ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███')                   
+    print (Fore.YELLOW + '  ███    ███     ███    ███ ███    █▀    ███▐██▀   ███▌ ███   ███   ███    █▀')            
+    print (Fore.YELLOW + ' ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀    ███▌ ███   ███  ▄███      ')                      
+    print (Fore.YELLOW + '▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄')                
+    print (Fore.YELLOW + '  ███    ███     ███    ███ ███    █▄    ███▐██▄   ███  ███   ███   ███    ███')                                
+    print (Fore.YELLOW + '  ███    ███     ███    ███ ███    ███   ███ ▀███▄ ███  ███   ███   ███    ███')                    
+    print (Fore.YELLOW + '  ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀') 
+    print ("CODED BY KILLER")
+
     print ("opcions")
     print ("\t1 - phishing(cheat website)")
-    print ("\t2 - sqlmap (vulnerabiliti scan")
+    print ("\t2 - sqlmap (vulnerability scan")
     print ("\t3 - Force Brute (facebook) ")
     print ("\t4 - nmap (port scanning)")
     print ("\t5 - PhoneXploit(device hacking)")
     print ("\t6 - python")
     print ("\t7 - python 2")
-    print ("\t8 - WPS killer (web dumping)")
+    print ("\t8 - WPS killer (wifi dumping)")
 
-    while True:
+while True:
 
-        menu()
+ menu()
 
-        opcionMenu = input("select a opcion >>")
+ opcionMenu = input("select a opcion >>")
 
-    if opcionMenu=="1":
+ if opcionMenu=="1":
         print ("Downloading...")
         os.system("git clone https://github.com/suljot/shellphish.git")
         os.system("cd shellphish")
@@ -39,7 +39,7 @@ def menu():
         input("\npress enter to back")
         os.system("clear")
     
-    if opcionMenu=="2":
+ if opcionMenu=="2":
         print("Downloading...")
         os.system("git clone https://github.com/sqlmapproject/sqlmap")
         os.system("cd sqlmap")
@@ -47,14 +47,14 @@ def menu():
         input("\npress enter to back")
         os.system("clear")
 
-    if opcionMenu=="3": 
+ if opcionMenu=="3": 
          print("Downloading...")
          os.system("git clone https://github.com/Gameye98/FBBrute")
          os.system(" python fbbrute.py")
          input("\npress enter to back")
          os.system("clear")
 
-    if opcionMenu=="4":
+ if opcionMenu=="4":
         print("Downloading...")
         os.system("git clone https://github.com/nmap/nmap")
         os.system("cd nmap")
@@ -64,29 +64,27 @@ def menu():
         input("\npress enter to back")
         os.system("clear")
 
-    if opcionMenu=="5":
+ if opcionMenu=="5":
         print("Downloading...")
         os.system("git clone https://github.com/dadi32/PhoneSploit")
         os.system("cd PhoneSploit")
         os.system("python2 main.py")
     
-    if opcionMenu=="6":
+ if opcionMenu=="6":
         print("Downloading...")
         os.system("git clone https://github.com/TheAlgorithms/Python")
         os.system("cd Python")
         os.system("./configure")
         os.system("make install")
 
-    if opcionMenu=="6":
+ if opcionMenu=="6":
         print("Downloading...")
         os.system("git clone https://github.com/FDX100/WPS-KILLER")
         os.system("cd WPS-KILLER")
         os.system("python WPS-KILLER.py")
     
-    if opcionMenu=="7":
+ if opcionMenu=="7":
         os.system("sudo dnf install python2")
-   
-    else:
-
-     print ("")
-     input("\ninvalid opcion press enter to back")
+ else:
+      print ("")
+      input("\ninvalid opcion press enter to back")
