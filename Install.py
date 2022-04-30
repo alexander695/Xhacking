ae82285fe6b525224347c56316b01552625d723d
PilowXe <78667474+alexander695@users.noreply.github.com>
2022-04-29 22:21:30 -0700

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,79 @@
+import os
+import time
+
+python_install = input("You have python installed?(Y or N):")
+
+if python_install=="Y":
+    print("okay the requirements will be installed ^^")
+    time.sleep(3)
+
+if python_install=="N":
+    print("Oh so the latest version of python will be installed :D")
+    time.sleep(3)
+    os.system("sudo apt-get install python3.10.4")
+
+if python_install=="y":
+    print("okay the requirements will be installed ^^")
+    time.sleep(3)
+
+if python_install=="n":
+    print("Oh so the latest version of python will be installed :D")
+    time.sleep(3)
+    os.system("sudo apt-get install python3.10.4")
+
+
+else:
+   Retry = input("MmMmMmM that is not a opcion Y or N?:")
+   
+   if Retry=="N":
+        print("Oh so the latest version of python will be installed")
+        time.sleep(3)
+        os.system("sudo apt-get install python3.10.4")
+   
+   if Retry=="Y":
+        print("okay the requirements will be installed")
+        time.sleep(3)
+   
+   if Retry=="y":
+       print("okay the requirements will be installed")
+       time.sleep(3)
+
+   if Retry=="n":
+       print("Oh so the latest version of python will be installed")
+       time.sleep(3)
+       os.system("sudo apt-get install python3.10.4")
+   
+retry3 = print("THAT IS NOT A OPCION, but from i can see you are a suck so i will install python")(time.sleep(4))(os.system("sudo apt-get install python3.10.4"))
+
+    
+    
+  
+os.system("pip install colorama")
+os.system("pip install time")
+
+print("Installation was completed sucefully ^^")
+
+Start = input("Do you want to start Xhacking? (Y or N):")
+
+if Start=="Y":
+    print("Ok! So if you like the proyect give a star ;)...")
+    time.sleep(2)
+    print("starting Xhacking...")
+    os.system("python3.9 Xhacking.py")
+
+if Start=="N":
+    print("Okay exiting!...\nGood bye! ^^")
+    time.sleep(2)
+    os.system("exit")
+
+if Start=="n":
+    print("Ok! So if you like the proyect give a star ;)...")
+    time.sleep(2)
+    print("starting Xhacking...")
+    os.system("python3.9 Xhacking.py")
+
+if Start=="n":
+    print("Okay exiting!...\nGood bye! ^^")
+    time.sleep(2)
+    os.system("exit")
+
