f063465bd887d52b10841409faa53d7536bc1c22
PilowXe <78667474+alexander695@users.noreply.github.com>
2022-05-15 22:47:14 -0700

    Update Install.py

--- Install.py
+++ Install.py
@@ -3,51 +3,32 @@ import time
 
 python_install = input("You have python installed?(Y or N):")
 
-if python_install=="Y":
-    print("okay the requirements will be installed ^^")
-    time.sleep(3)
-
 if python_install=="N":
     print("Oh so the latest version of python will be installed :D")
     time.sleep(3)
-    os.system("sudo apt-get install python3.10.4")
+    os.system("sudo apt-get install python3.9")
+    os.system("sudo apt-get install python3-pip")
+
+if python_install=="n":
+    print("Oh so the latest version of python will be installed :D")
+    time.sleep(3)
+    os.system("sudo apt-get install python3.9")
+    os.system("sudo apt-get install python3-pip")
 
 if python_install=="y":
     print("okay the requirements will be installed ^^")
     time.sleep(3)
 
-if python_install=="n":
-    print("Oh so the latest version of python will be installed :D")
+if python_install=="Y":
+    print("okay the requirements will be installed ^^")
     time.sleep(3)
-    os.system("sudo apt-get install python3.10.4")
+
 
 
 else:
-   Retry = input("MmMmMmM that is not a opcion Y or N?:")
-   
-   print("THAT IS NOT A OPCION, but only i will install python")
-   time.sleep(4)
-   os.system("sudo apt-get install python3.10.4")
-   
-   if Retry=="N":
-        print("Oh so the latest version of python will be installed")
-        time.sleep(3)
-        os.system("sudo apt-get install python3.10.4")
-   
-   if Retry=="Y":
-        print("okay the requirements will be installed")
-        time.sleep(3)
-   
-   if Retry=="y":
-       print("okay the requirements will be installed")
-       time.sleep(3)
-
-   if Retry=="n":
-       print("Oh so the latest version of python will be installed")
-       time.sleep(3)
-       os.system("sudo apt-get install python3.10.4")
-
-
+    print("idk what are you trying to tell but y will install python")
+    os.system("sudo apt install python3.9")
+    os.system("sudo apt-get install python3-pip")
   
 os.system("pip install colorama")
 
@@ -55,13 +36,12 @@ print("Installation was completed sucefully ^^")
 
 Start = input("Do you want to start Xhacking? (Y or N):")
 
-if Start=="Y":
-    print("Ok! So if you like the proyect give a star ;)...")
-    time.sleep(2)
-    print("starting Xhacking...")
-    os.system("python3.9 Xhacking.py")
-
 if Start=="N":
+    print("Okay exiting!...\nGood bye! ^^")
+    time.sleep(2)
+    os.system("exit")
+
+if Start=="n":
     print("Okay exiting!...\nGood bye! ^^")
     time.sleep(2)
     os.system("exit")
@@ -72,10 +52,11 @@ if Start=="y":
     print("starting Xhacking...")
     os.system("python3.9 Xhacking.py")
 
-if Start=="n":
-    print("Okay exiting!...\nGood bye! ^^")
+if Start=="Y":
+    print("Ok! So if you like the proyect give a star ;)...")
     time.sleep(2)
-    os.system("exit")
+    print("starting Xhacking...")
+    os.system("python3.9 Xhacking.py")
 
 
 
